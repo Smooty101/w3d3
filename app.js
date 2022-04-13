8d97cdb308a0ceea942830ea488ab03ca2f33020
Smooty Ziah <ehydlebeerg2@gmail.com>
2022-04-13 16:33:43 -0400

    new push

---
+++ app.js
@@ -0,0 +1,14 @@
+var a = 10;
+var b = 20;
+var c = 30;
+var d = 40;
+
+
+console.log(a+b);
+console.log(c+d);
+console.log(a-b);
+console.log(c-d);
+console.log(a*b);
+console.log(c*d);
+console.log(a/b);
+console.log(c/d);
