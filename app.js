442f5410f6f299f362e5559b62195f118f6ce2ab
Smooty Ziah <ehydlebeerg2@gmail.com>
2022-04-14 10:08:03 -0400

    adding css file

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ var a = 10;
 var b = 20;
 var c = 30;
 var d = 40;
+var e = 50;
+var f = 60; 
+var g = 70;
+var h = 80;
+var i = 90;
+var j = 100;
+
+console.log(f++)
+console.log(g++)
+console.log(h--)
+console.log(i--)
+
 
 
 console.log(a+b);
